626bb4443dcd7b2c0ca7445afcb7e049d7c396fd
nduje <dujerancic@gmail.com>
2025-11-30 05:44:08 +0100

    Finish initial User Application implementation

--- Internship-4-OOP2.Application/Common/Model/RequestHandler.cs
+++ Internship-4-OOP2.Application/Common/Model/RequestHandler.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Internship_4_OOP2.Application.Common.Model
+﻿namespace Internship_4_OOP2.Application.Common.Model
 {
-    internal class RequestHandler
+    public abstract class RequestHandler<TRequest, TResult> where TRequest : class where TResult : class
     {
+        public Guid RequestId => Guid.NewGuid();
+
+        public async Task<Result<TResult>> ProcessAuthorizedRequestAsync(TRequest request)
+        {
+            var result = new Result<TResult>();
+
+            if (await IsAuthorized() == false)
+            {
+                result.SetUnauthorizedResult();
+                return result;
+            }
+
+            await HandleRequest(request, result);
+
+            return result;
+        }
+
+        protected abstract Task<Result<TResult>> HandleRequest(TRequest request, Result<TResult> result);
+
+        protected abstract Task<bool> IsAuthorized();
     }
 }
--- Internship-4-OOP2.Domain/Persistence/Users/IUserRepository.cs
+++ Internship-4-OOP2.Domain/Persistence/Users/IUserRepository.cs
@@ -2,7 +2,7 @@
 
 namespace Internship_4_OOP2.Domain.Persistence.Users
 {
-    internal interface IUserRepository
+    public interface IUserRepository
     {
         Task<User> GetById(int id);
     }
