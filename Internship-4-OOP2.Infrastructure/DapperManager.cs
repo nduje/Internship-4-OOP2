be712a2391c66b10dedfce9a6900f221257a479c
nduje <dujerancic@gmail.com>
2025-11-30 07:33:40 +0100

    Configure Dapper

---
+++ Internship-4-OOP2.Infrastructure/DapperManager.cs
@@ -0,0 +1,47 @@
+﻿
+using Dapper;
+using Internship_4_OOP2.Application.Common.Model;
+using Npgsql;
+
+namespace Internship_4_OOP2.Infrastructure
+{
+    internal sealed class DapperManager : IDapperManager
+    {
+        private readonly string _connectionString;
+
+        public DapperManager(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        private NpgsqlConnection CreateConnection()
+        {
+            return new NpgsqlConnection(_connectionString);
+        }
+
+        public async Task ExecuteAsync(string sql, object? param = null)
+        {
+            using var connection = CreateConnection();
+            await connection.OpenAsync();
+
+            await connection.ExecuteAsync(sql, param);
+        }
+
+        public async Task<IReadOnlyList<T>> QueryAsync<T>(string sql, object? param = null)
+        {
+            using var connection = CreateConnection();
+            await connection.OpenAsync();
+
+            var result = await connection.QueryAsync<T>(sql, param);
+            return result.ToList();
+        }
+
+        public async Task<T?> QuerySingleAsync<T>(string sql, object? param = null)
+        {
+            using var connection = CreateConnection();
+            await connection.OpenAsync();
+
+            return await connection.QuerySingleAsync<T>(sql, param);
+        }
+    }
+}
---
+++ Internship-4-OOP2.Infrastructure/IDapperManager.cs
@@ -0,0 +1,11 @@
+﻿namespace Internship_4_OOP2.Infrastructure
+{
+    public interface IDapperManager
+    {
+        Task<IReadOnlyList<T>> QueryAsync<T>(string sql, object? param = null);
+        
+        Task<T?> QuerySingleAsync<T>(string sql, object? param = null);
+
+        Task ExecuteAsync(string sql, object? param = null);
+    }
+}
